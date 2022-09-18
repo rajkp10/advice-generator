10b0483ba08154d5e85db894f15f243f0f7f3076
Raj <patelraj10401@gmail.com>
2022-09-18 23:07:02 +0530

    letter spacing added to advise number

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,8 +8,8 @@ function Card({ adviceObj, handleFetch }) {
   return (
     <div className="container max-w-xs md:max-w-md p-5 flex flex-col justify-between items-center space-y-8">
       <div>
-        <span className="text-NeonGreen tracking-widest">
-          Advise #{adviceObj.id}
+        <span className="text-NeonGreen tracking-[0.25rem]">
+          Advice #{adviceObj.id}
         </span>
       </div>
       <Advice advice={adviceObj.advice} />
